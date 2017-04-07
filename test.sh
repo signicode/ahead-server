b47f5220da7953b891c6a9b5dad224425ea2d3d7
Budleigh Salterton driving a Miura <budleigh.salterton@gmail.com>
2017-04-07 17:43:57 +0200

    updated version, cdn stratch

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
-Ahead LSH
------------
-
-Sub second latency HLS and DASH Server and CDN
+#!/bin/bash
 
 ffmpeg -thread_queue_size 32 \
         -f v4l2 -input_format mjpeg -video_size 640x360 -framerate 30 -i /dev/video0 \
@@ -9,11 +6,6 @@ ffmpeg -thread_queue_size 32 \
         -c:v libx264 -b:v 1m -preset fast -pix_fmt yuv420p -tune zerolatency -x264-params keyint=15:min-keyint=15 -r 25\
         -c:a aac -ac 2 -b:a 96k \
         -f ssegment -segment_list_flags live \
-            -segment_list_type csv -segment_time .5 -segment_list pipe:1 \
+            -segment_list_type csv -segment_time 1 -segment_list pipe:1 \
             -segment_wrap 99 work/out%02d.ts \
     | node ./ahead-server.js
-
-License
---------
-
-THIS IS NOT LICENSED. YOU HAVE NO RIGHT TO USE THIS UNLESS YOU HAVE A WRITTEN PERMISSION FROM THE RIGHTS OWNER!
