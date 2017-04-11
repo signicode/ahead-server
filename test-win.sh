2055b4f537513fcac5fd9931a103310bc12ec1fc
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-04-11 10:14:48 +0200

    windows example

---
+++ test-win.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ ffmpeg -video_size 640x480 -framerate 25 -f dshow -i video="USB2.0 HD UVC WebCam" \
+        -r 25 \
+        -c:v libx264 -b:v 1m -preset fast -pix_fmt yuv420p -tune zerolatency -x264-params keyint=25:min-keyint=25 \
+        -c:a aac -ac 2 -b:a 96k \
+        -f ssegment -segment_list_flags live \
+            -segment_list_type csv -segment_time 1 -segment_list pipe:1 \
+            work/out%03d.ts \
+    | node ./ahead-server.js
