917c47757979dc5894acf22aaba7c2a8d25407d3
Budleigh Salterton driving a Miura <budleigh.salterton@gmail.com>
2017-05-23 22:48:20 +0200

    add publish endpoint

--- test-publish.sh
+++ test-publish.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 ffmpeg -thread_queue_size 32 \
-        -f v4l2 -input_format mjpeg -video_size 640x360 -framerate 25 -i /dev/video0 \
-        -f alsa -i hw:3 -map 0 \
-        -r 25
+	-listen 1 -rtmp_app live -i rtmp://0.0.0.0/\
+        -r 25\
         -c:v libx264 -b:v 1m -preset fast -pix_fmt yuv420p -tune zerolatency -x264-params keyint=25:min-keyint=25\
         -c:a aac -ac 2 -b:a 96k \
         -f ssegment -segment_list_flags live \
